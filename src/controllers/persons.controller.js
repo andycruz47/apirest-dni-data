6c57285dd60d5d841091bd3053ad0a8ec74fac0c
Andy Cruz <andy10chicho@gmail.com>
2022-11-12 23:50:25 -0500

    First commit

--- src/controllers/persons.controller.js
+++ src/controllers/persons.controller.js
@@ -1,23 +1,24 @@
 import { pool } from "../db.js";
 
-export const getEmployees = async (req, res) => {
+export const getpersons = async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT * FROM employee");
+    const [rows] = await pool.query("SELECT * FROM persona");
     res.json(rows);
   } catch (error) {
     return res.status(500).json({ message: "Something goes wrong" });
   }
 };
 
-export const getEmployee = async (req, res) => {
+export const getperson = async (req, res) => {
   try {
     const { id } = req.params;
-    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+    console.log(req.params)
+    const [rows] = await pool.query("SELECT * FROM persona WHERE dni = ?", [
       id,
     ]);
 
     if (rows.length <= 0) {
-      return res.status(404).json({ message: "Employee not found" });
+      return res.status(404).json({ message: "person not found" });
     }
 
     res.json(rows[0]);
@@ -26,13 +27,13 @@ export const getEmployee = async (req, res) => {
   }
 };
 
-export const deleteEmployee = async (req, res) => {
+export const deleteperson = async (req, res) => {
   try {
     const { id } = req.params;
-    const [rows] = await pool.query("DELETE FROM employee WHERE id = ?", [id]);
+    const [rows] = await pool.query("DELETE FROM persona WHERE id = ?", [id]);
 
     if (rows.affectedRows <= 0) {
-      return res.status(404).json({ message: "Employee not found" });
+      return res.status(404).json({ message: "person not found" });
     }
 
     res.sendStatus(204);
@@ -41,11 +42,11 @@ export const deleteEmployee = async (req, res) => {
   }
 };
 
-export const createEmployee = async (req, res) => {
+export const createperson = async (req, res) => {
   try {
     const { name, salary } = req.body;
     const [rows] = await pool.query(
-      "INSERT INTO employee (name, salary) VALUES (?, ?)",
+      "INSERT INTO persona (name, salary) VALUES (?, ?)",
       [name, salary]
     );
     res.status(201).json({ id: rows.insertId, name, salary });
@@ -54,20 +55,20 @@ export const createEmployee = async (req, res) => {
   }
 };
 
-export const updateEmployee = async (req, res) => {
+export const updateperson = async (req, res) => {
   try {
     const { id } = req.params;
     const { name, salary } = req.body;
 
     const [result] = await pool.query(
-      "UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?",
+      "UPDATE persona SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?",
       [name, salary, id]
     );
 
     if (result.affectedRows === 0)
-      return res.status(404).json({ message: "Employee not found" });
+      return res.status(404).json({ message: "person not found" });
 
-    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+    const [rows] = await pool.query("SELECT * FROM persona WHERE id = ?", [
       id,
     ]);
 
---
+++ src/routes/persons.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  createperson,
+  deleteperson,
+  getperson,
+  getpersons,
+  updateperson,
+} from "../controllers/persons.controller.js";
+
+const router = Router();
+
+// GET all persons
+router.get("/persons", getpersons);
+
+// GET An person
+router.get("/person/:id", getperson);
+
+// DELETE An person
+router.delete("/person/:id", deleteperson);
+
+// INSERT An person
+router.post("/person", createperson);
+
+router.patch("/person/:id", updateperson);
+
+export default router;
